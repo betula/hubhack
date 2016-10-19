f655c3845369c2e16d039a3d016957f1e18643d3
Viacheslav Bereza <betula.icons8@gmail.com>
2016-10-19 19:24:26 +0300

    First start

--- hubhack/AppDelegate.swift
+++ hubhack/AppDelegate.swift
@@ -7,21 +7,79 @@
 //
 
 import Cocoa
+import Foundation
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var statusMenu: NSMenu!
 
+    let statusItem = NSStatusBar.system().statusItem(withLength:NSVariableStatusItemLength)
 
+    var timer: Timer?
+    
+    var started = false
+    let title = "H."
+    let titleStarted = "H->"
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        statusItem.title = title
+        statusItem.menu = statusMenu
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
     }
-
+    
+    @IBAction func quitClicked(_ sender: NSMenuItem) {
+        NSApplication.shared().terminate(self)
+    }
+    
+    @IBAction func startClicked(_ sender: NSMenuItem) {
+        if (started) {
+            return
+        }
+        started = true
+        statusItem.title = titleStarted
+        timer = Timer.scheduledTimer(
+            timeInterval: 5,
+            target:self,
+            selector: #selector(AppDelegate.tick),
+            userInfo: nil,
+            repeats: true
+        )
+    }
+    @IBAction func stopClicked(_ sender: NSMenuItem) {
+        if (!started) {
+            return
+        }
+        started = false
+        statusItem.title = title
+        timer?.invalidate()
+    }
+    
+    func tick() {
+        let cursor = NSEvent.mouseLocation()
+        
+        for dx in -1...0 {
+            for dy in -1...0 {
+                
+                let mouseMoveEvent = CGEvent(
+                    mouseEventSource: nil,
+                    mouseType: .mouseMoved,
+                    mouseCursorPosition: CGPoint(
+                        x: Double(Float(cursor.x) + Float(dx)),
+                        y: Double(Float(cursor.y) + Float(dy))
+                    ),
+                    mouseButton: CGMouseButton.left
+                )
+                mouseMoveEvent?.post(tap: .cghidEventTap)
+                usleep(200_000)
+            }
+        }
+        
+        
+        
+    }
 
 }
 
